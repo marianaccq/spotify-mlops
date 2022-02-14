3ef404c39c5b27e4ac11c30e9e1bca63ba5e4f3b
mariana <mariana.queiroz.110@ufrn.edu.br>
2022-02-14 20:06:37 -0300

    criando o primeiro artefato download_data

---
+++ download/download_data.py
@@ -0,0 +1,86 @@
+"""
+Creator: Mariana
+Date: Fev. 2022
+Download the raw data
+"""
+import argparse
+import logging
+import pathlib
+import wandb
+import requests
+import tempfile
+import opendatasets as od
+
+
+# configure logging
+logging.basicConfig(level=logging.INFO,
+                    format="%(asctime)s %(message)s",
+                    datefmt='%d-%m-%Y %H:%M:%S')
+
+# reference for a logging obj
+logger = logging.getLogger()
+
+
+def process_args(args):
+    # Download file, streaming so we can download files larger than
+    # the available memory. We use a named temporary file that gets
+    # destroyed at the end of the context, so we don't leave anything
+    # behind and the file gets removed even in case of errors
+    logger.info(f"Downloading {args.file_url} ...")
+    with tempfile.NamedTemporaryFile(mode='wb+') as fp:
+
+        logger.info("Creating run")
+        with wandb.init(project='spotify_mlops', job_type="download_data") as run:
+            od.download(args.file_url)
+            with open("./dataset-of-songs-in-spotify/genres_v2.csv", 'rb') as file:
+                fp.writelines(file)
+
+            # Make sure the file has been written to disk before uploading
+            # to W&B
+            fp.flush()
+
+            logger.info("Creating artifact")
+            artifact = wandb.Artifact(
+                name=args.artifact_name,
+                type=args.artifact_type,
+                description=args.artifact_description,
+                metadata={'original_url': args.file_url}
+            )
+            artifact.add_file(fp.name, name=args.artifact_name)
+
+            logger.info("Logging artifact")
+            run.log_artifact(artifact)
+
+            artifact.wait()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Download a file and upload it as an artifact to W&B", fromfile_prefix_chars="@"
+    )
+
+    parser.add_argument(
+        "--file_url", type=str, help="URL to the input file", required=True
+    )
+
+    parser.add_argument(
+        "--artifact_name", type=str, help="Name for the artifact", required=True
+    )
+
+    parser.add_argument(
+        "--artifact_type", type=str, help="Type for the artifact", required=True
+    )
+
+    parser.add_argument(
+        "--artifact_description",
+        type=str,
+        help="Description for the artifact",
+        required=True,
+    )
+
+    ARGS = parser.parse_args()
+
+    process_args(ARGS)
+    
+    
+# mlflow run . -P file_url=https://www.kaggle.com/mrmorj/dataset-of-songs-in-spotify?select=genres_v2.csv -P artifact_name=spotify_mlops -P artifact_type=raw_data -P artifact_description="This is a Dataset of songs in Spotify"
